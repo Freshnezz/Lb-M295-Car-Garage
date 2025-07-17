7439bed7b888a5206e7081cc5d096c9f29acbe68
Freshnezz <marc.augsburger@gmail.com>
2025-07-17 18:26:27 +0200

    initial commit from intellij

---
+++ src/main/java/com/wiss/cargarage/CarGarageApplication.java
@@ -0,0 +1,13 @@
+package com.wiss.cargarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarGarageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarGarageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wiss/cargarage/CarGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiss.cargarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarGarageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
